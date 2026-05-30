224f5ec7f1ad6b74c93fdd030e0d788a7ac741cd
Josh Chen <josh@joshchen.dev>
2026-05-31 01:59:23 +0900

    exercise 11-12 done

--- app/services/blogs.ts
+++ app/services/blogs.ts
@@ -1,15 +1,17 @@
 import { db } from "@/db"
 import { blogs } from "@/db/schema"
 import { eq, sql } from "drizzle-orm"
+import { getCurrentUser } from "./session"
 
 export const getBlogs = async () => {
   return db.query.blogs.findMany()
 }
 
 export const addBlog = async (title: string, author: string, url: string) => {
-  const user = await db.query.users.findFirst({
-    orderBy: sql`RANDOM()`
-  })
+  const user = await getCurrentUser()
+  if (!user) {
+    throw new Error("Not logged in")
+  }
 
   await db.insert(blogs).values({ title, author, url, userId: user!.id })
 }
